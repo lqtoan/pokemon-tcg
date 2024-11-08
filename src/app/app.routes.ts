73cbc53aad2fa69e421afadf4fdff82e69d46743
lqtoan <lqtoan37@gmail.com>
2024-11-08 15:43:11 +0700

    update UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { PokemonSetListComponent } from './components/pokemon-set-list/pokemon-set-list.component';
+import { PokemonCardListComponent } from './components/pokemon-card-list/pokemon-card-list.component';
 
 export const routes: Routes = [
   { path: 'tcg', component: PokemonSetListComponent },
-  // { path: '', component: PokemonListComponent },
-  { path: '', redirectTo: '/tcg', pathMatch: 'full' },
-  { path: '**', redirectTo: '/tcg' },
+  { path: 'pokedex', component: PokemonCardListComponent },
+  // { path: '', redirectTo: '/tcg', pathMatch: 'full' },
+  // { path: '**', redirectTo: '/tcg' },
 ];
